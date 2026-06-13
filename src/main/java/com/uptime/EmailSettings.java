0937f90ef69bb32adef462721c9d3ccc74fac249
femi <femifeel12@gmail.com>
2026-06-13 18:37:25 +0100

    feat: added email settings

---
+++ src/main/java/com/uptime/EmailSettings.java
@@ -0,0 +1,10 @@
+package com.uptime;
+
+public class EmailSettings {
+    public String smtpHost;
+    public int smtpPort;
+    public String smtpUser;
+    public String smtpPass;
+    public String fromEmail;
+    public String toEmail;
+}
